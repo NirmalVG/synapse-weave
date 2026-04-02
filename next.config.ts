2fa34483d669ae898f31714210329edb08bbe628
NIRMAL V G <Nirmalvg97@gmail.com>
2026-04-02 10:53:23 +0530

    pwa added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import withSerwistInit from "@serwist/next"
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const isDev = process.env.NODE_ENV === "development"
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  disable: isDev,
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+// THE FIX:
+// If we are coding locally (dev), use the standard config so Turbopack works.
+// If we are deploying (build), wrap it with Serwist to generate the PWA.
+export default isDev ? nextConfig : withSerwist(nextConfig)
